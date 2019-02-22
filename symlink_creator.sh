f40a8f5c03c2f86d6cb2884405e98b4ad7d89c0b
Soroush Saffari <sor.saffari@gmail.com>
2019-02-22 19:07:05 +0000

    extends symlink-creator to work with new symlinks

--- symlink_creator.sh
+++ symlink_creator.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-set -xe
+# set -xe
 
 absolute_path_prefix=$(pwd)
+
+whitelist_dirs=(client_api concept_api views) # only create symlinks if the file is contained within the given directory names
 
 for original in $(find docs -name '*.yml'); do
     symlink=${original#"docs/"} # remove prefix
@@ -17,11 +19,17 @@ for original in $(find docs -name '*.yml'); do
     absolute_symlink="$absolute_path_prefix/_data/$symlink" # .yml symlink path
     absolute_original="$absolute_path_prefix/$original" # original .yml file path
 
-    mkdir -p "$(dirname $absolute_symlink)" # create the symlink's path if non-existent (-p)
-    echo $absolute_original
+    IFS='/' read -r -a splited_symlink <<< "$symlink" # split symlink into array by "/" as delimeter
+    dir=${splited_symlink[0]} # get the directory to check against whitelist_dirs
 
-    ln -s $absolute_original $absolute_symlink
-    if ! grep -q "./_data/$symlink" .gitignore; then
-        echo "\n./_data/$symlink" >> .gitignore
+    if [[ " ${whitelist_dirs[@]} " =~ " ${dir} " ]]; then # allowed to create symlink
+        mkdir -p "$(dirname $absolute_symlink)" # create the symlink's path if non-existent (-p)
+        ln -s $absolute_original $absolute_symlink
+
+        # add created symlinks to .gitignore
+        # because the counterpart of this bash script creates symlinks for deployment to heroku
+        if ! grep -q "_data/$dir" .gitignore; then
+            echo "_data/$dir" >> .gitignore
+        fi
     fi
 done
