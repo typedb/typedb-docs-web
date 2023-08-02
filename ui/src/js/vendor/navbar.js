2fb2c9f430915a360b606269abc665ec09d9867a
Alex Walker <alex@vaticle.com>
2023-08-02 17:08:58 +0100

    Revert "Try reverting to Jul 14th"
    This reverts commit 4868dc1df81df70b2262f95386fe85542f91cb0c.

--- ui/src/js/vendor/navbar.js
+++ ui/src/js/vendor/navbar.js
@@ -1,8 +1,37 @@
 let hoveredMenuPanelIndex = undefined;
 let hoveredMenuItemIndex = undefined;
 
+;(function () {
+  let mobileMenuBtn = document.querySelector("button[_ngcontent-bfa-c139]")
+  mobileMenuBtn.addEventListener('click', function(e){
+    e.target.closest("button[_ngcontent-bfa-c139]").classList.toggle("tb-opened");
+    let topbarMenuMobile = document.querySelector("td-topbar-menu-mobile[_ngcontent-bfa-c139]")
+    topbarMenuMobile.toggleAttribute("hidden");
+
+
+    let atagExpandableList = document.querySelectorAll("a[_ngcontent-bfa-c141]") || []
+    if(atagExpandableList.length>0){
+      for(let atag of atagExpandableList){
+        atag.addEventListener('click', function(event){
+          let parent = event.target.closest('li')
+          let aEle = parent.querySelector('a[_ngcontent-bfa-c141]')
+          aEle.classList.toggle("tm-expanded");
+
+          let submenu = parent.querySelector("td-topbar-menu-panel-mobile[_ngcontent-bfa-c141]")
+          submenu.toggleAttribute("hidden");
+
+        })
+      }
+
+    }
+  })
+
+
+
+})()
+
 function updateMenuPanelVisibility() {
-    for (const i of [1, 2, 3]) {
+    for (const i of [1, 2, 3,4]) {
         if ([hoveredMenuItemIndex, hoveredMenuPanelIndex].includes(i)) {
             document.getElementById(`topbarMenuPanel${i}`).removeAttribute('hidden');
         } else {
@@ -11,7 +40,7 @@ function updateMenuPanelVisibility() {
     }
 }
 
-for (const i of [1, 2, 3]) {
+for (const i of [1, 2, 3,4]) {
     document.getElementById(`topbarMenuItem${i}`).addEventListener('mouseenter', () => {
         hoveredMenuItemIndex = i;
         hoveredMenuPanelIndex = undefined;
