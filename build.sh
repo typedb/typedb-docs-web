d75420632fe225d347b62813e021b10e1b97528a
Alex Walker <alex@vaticle.com>
2023-08-03 11:41:45 +0100

    Try using --to-dir prop

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cd ../ui && gulp bundle && cd ../docs && URL=https://development.typedb.com/docs npx --yes antora --redirect-facility netlify antora-playbook.yml
+cd ../ui && gulp bundle && cd ../docs && URL=https://development.typedb.com/docs npx --yes antora --to-dir docs antora-playbook.yml
