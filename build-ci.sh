ef07a2cc8e97748ff935d726fe1db3893e0b9dde
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-09-07 13:04:01 +0200

    Configures environments

--- build-ci.sh
+++ build-ci.sh
@@ -1,1 +1,1 @@
-cd ui && gulp bundle && cd .. && npx --yes antora antora-playbook.yml --url=https://typedb.com/docs --html-url-extension-style=drop
+npm run build-ui && npx antora antora-playbook.yml --url=https://typedb.com/docs --html-url-extension-style=drop --key google_analytics=G-SNVZCNLJ9R --key hubspot=4332244
