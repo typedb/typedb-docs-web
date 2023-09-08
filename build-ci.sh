edbd297b45288c67129048180c35398ea09095c7
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-09-08 14:51:29 +0200

    Uses env variables

--- build-ci.sh
+++ build-ci.sh
@@ -1,1 +1,1 @@
-npm run build-ui && npx antora antora-playbook.yml --url=https://typedb.com/docs --html-url-extension-style=drop --key google_analytics=G-SNVZCNLJ9R --key hubspot=4332244
+npm run build-ui && npx antora antora-playbook.yml --url=${ANTORA_SITE_URL} --html-url-extension-style=drop --key google_analytics=${ANTORA_GOOGLE_ANALYTICS_KEY} --key hubspot=${ANTORA_HUBSPOT_KEY}
