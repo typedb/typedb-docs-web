eeb75fac1c8748b02f1d4571e20960d93eb7dacf
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-09-11 14:30:54 +0200

    Make build scripts behave correctly in all environments, and run analytics only in production (#167)
    ## What is the goal of this PR?
    
    Previously npm scripts were not working on Windows. Additionally some
    tracking codes were added to all environments.
    Now docs can be built on Windows. Tracking codes are added only on CI.
    
    
    ## What are the changes implemented in this PR?
    
    - moves site keys to command param,
    - moves `html_extension_style` option to command param,
    - replaces locally run scripts to be inline npm scripts,
    - uses gulp locally without global install,
    - moves hubspot portal id to antora keys.

--- build-ci.sh
+++ build-ci.sh
@@ -1,1 +1,1 @@
-cd ui && gulp bundle && cd .. && npx --yes antora antora-playbook.yml --url=https://typedb.com/docs --html-url-extension-style=drop
+npm run build-ui && npx antora antora-playbook.yml --url=${ANTORA_SITE_URL} --html-url-extension-style=drop --key google_analytics=${ANTORA_GOOGLE_ANALYTICS_KEY} --key hubspot=${ANTORA_HUBSPOT_KEY}
