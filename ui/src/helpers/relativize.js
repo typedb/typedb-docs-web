8de819ba14ad065415ec5df2e0b6b0c888faec59
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-11-07 13:54:46 +0100

    Fixes relativize for one level up

--- ui/src/helpers/relativize.js
+++ ui/src/helpers/relativize.js
@@ -1,24 +1,25 @@
-'use strict'
+"use strict";
 
-const { posix: path } = require('path')
+const { posix: path } = require("path");
 
 module.exports = (to, from, ctx) => {
-  if (!to) return '#'
+  if (!to) return "#";
   // NOTE only legacy invocation provides both to and from
-  if (!ctx) from = (ctx = from).data.root.page.url
-  if (to.charAt() !== '/') return to
-  if (!from) return (ctx.data.root.site.path || '') + to
-  let hash = ''
-  const hashIdx = to.indexOf('#')
+  if (!ctx) from = (ctx = from).data.root.page.url;
+  if (to.charAt() !== "/") return to;
+  if (!from) return (ctx.data.root.site.path || "") + to;
+  let hash = "";
+  const hashIdx = to.indexOf("#");
   if (~hashIdx) {
-    hash = to.substr(hashIdx)
-    to = to.substr(0, hashIdx)
+    hash = to.substr(hashIdx);
+    to = to.substr(0, hashIdx);
   }
   return to === from
-    ? hash || (isDir(to) ? './' : path.basename(to))
-    : (path.relative(path.dirname(from + '.'), to) || '.') + (isDir(to) ? '/' + hash : hash)
-}
+    ? hash || (isDir(to) ? "./" : path.basename(to))
+    : path.relative(path.dirname(from + "."), to + "_").slice(0, -1) ||
+        "." + (isDir(to) ? "/" + hash : hash);
+};
 
-function isDir (str) {
-  return str.charAt(str.length - 1) === '/'
+function isDir(str) {
+  return str.charAt(str.length - 1) === "/";
 }
