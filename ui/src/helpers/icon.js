1a76faa5f665af2156fe9cef482da130c5db1b1d
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-11-13 10:54:12 +0100

    Uses dashed icon names

--- ui/src/helpers/icon.js
+++ ui/src/helpers/icon.js
@@ -3,8 +3,7 @@
 const fs = require("fs");
 
 module.exports = name => {
-  const fileName = name.replace("_", "-");
-  const commonPath = `./ui/node_modules/typedb-web-common/src/icons/${fileName}.svg`;
+  const commonPath = `./ui/node_modules/typedb-web-common/src/icons/${name}.svg`;
   const svgContent = fs.readFileSync(commonPath, "utf-8");
   return svgContent;
 };
