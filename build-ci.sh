ac6ab4237520a0f53cfb7595ee6714eafcad0666
Alex Walker <alex@vaticle.com>
2023-11-15 17:44:05 +0000

    Minimal PushFeedback integration

--- build-ci.sh
+++ build-ci.sh
@@ -1,1 +1,1 @@
-pnpm build-ui && npx antora antora-playbook.yml --url=${ANTORA_SITE_URL} --html-url-extension-style=drop --key google_analytics=${ANTORA_GOOGLE_ANALYTICS_KEY} --key hubspot=${ANTORA_HUBSPOT_KEY}
+pnpm build-ui && npx antora antora-playbook.yml --url=${ANTORA_SITE_URL} --html-url-extension-style=drop --key google_analytics=${ANTORA_GOOGLE_ANALYTICS_KEY} --key hubspot=${ANTORA_HUBSPOT_KEY} --key push_feedback=${ANTORA_PUSH_FEEDBACK_KEY}
