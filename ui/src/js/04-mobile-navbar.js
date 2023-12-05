b3ebc2f409b140c449d73eefa1007c7fdce49e48
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-12-05 15:11:43 +0100

    Implements responsive design

---
+++ ui/src/js/04-mobile-navbar.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+
+    const navButton = document.querySelector(".nav-toggle-button");
+    const nav = document.querySelector(".nav");
+    if (!navButton || !nav) return;
+
+    navButton.addEventListener("click", (ev) => {
+        document.documentElement.classList.toggle("is-clipped--navbar");
+        nav.classList.toggle("nav-opened");
+    });
+})();
