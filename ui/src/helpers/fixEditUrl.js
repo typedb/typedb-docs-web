92eb173ccdb25ecedb9ad0f2fa2f7c70524a891e
Vladimir Izmalkov <48120135+izmalk@users.noreply.github.com>
2024-03-05 11:42:39 +0000

    Fixing the buttons tooltips (#212)
    ## What is the goal of this PR?
    
    To fix tooltips for the buttons underneath the in-page table of contents
    on the right.
    
    ## What are the changes implemented in this PR?
    
    Style fixes mostly.
    
    ---------
    
    Co-authored-by: cxdorn <christophxdorn@gmail.com>

--- ui/src/helpers/fixEditUrl.js
+++ ui/src/helpers/fixEditUrl.js
@@ -4,7 +4,7 @@ module.exports = (...args) => {
   const numArgs = args.length;
   if (numArgs === 2 && args[0]) {
     return args[0].replace('/development/content/', '/development/');
-  }
+  } 
   if (numArgs < 2) throw new Error('{{fixEditUrl}} helper expects at least 1 arguments');
   return "";
 }
