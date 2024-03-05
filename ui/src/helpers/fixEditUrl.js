ba1429024b46f54347476c5ac6d26eaff95a6489
Vladimir Izmalkov <vladimir@vaticle.com>
2024-03-05 09:52:07 +0000

    Hotfix: Fix build with new style

--- ui/src/helpers/fixEditUrl.js
+++ ui/src/helpers/fixEditUrl.js
@@ -1,9 +1,10 @@
 'use strict'
 
 module.exports = (...args) => {
-  const numArgs = args.length
-  if (numArgs === 2) return args[0].replace('/development/content/', '/development/');
-  if (numArgs < 2) throw new Error('{{fixEditUrl}} helper expects at least 1 arguments')
-  console.log('fix edit URL failed');
-  return args[0];
+  const numArgs = args.length;
+  if (numArgs === 2 && args[0]) {
+    return args[0].replace('/development/content/', '/development/');
+  }
+  if (numArgs < 2) throw new Error('{{fixEditUrl}} helper expects at least 1 arguments');
+  return "";
 }
