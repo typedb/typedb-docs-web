a97d33cacb72f4becebddfc1fbbdcfe4bc4094bf
Alex Walker <alex@typedb.com>
2025-09-03 21:31:09 +0100

    Fix broken links in 2.x docs

--- build-ci.sh
+++ build-ci.sh
@@ -1,1 +1,1 @@
-pnpm build-ui && npx antora antora-playbook.yml --url=${ANTORA_SITE_URL} --html-url-extension-style=drop --key google_analytics=${ANTORA_GOOGLE_ANALYTICS_KEY} --key posthog=${ANTORA_POSTHOG_KEY} --key cio=${ANTORA_CIO_KEY} --key intercom=${ANTORA_INTERCOM_KEY} --key marker_io=${ANTORA_MARKER_IO_KEY}
+pnpm build-ui && npx antora antora-playbook.yml --url=${ANTORA_SITE_URL} --html-url-extension-style=drop --key google_analytics=${ANTORA_GOOGLE_ANALYTICS_KEY} --key posthog=${ANTORA_POSTHOG_KEY} --key cio=${ANTORA_CIO_KEY} --key intercom=${ANTORA_INTERCOM_KEY}
