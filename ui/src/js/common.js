bb321732685163114f55911b9cd975a7324bdad1
sam-butcher <samuel@typedb.com>
2025-11-05 10:12:25 +0000

    update nav

--- ui/src/js/common.js
+++ ui/src/js/common.js
@@ -2,4 +2,4 @@ import "typedb-web-common/lib/prism";
 import { initCustomScrollbars } from "typedb-web-common/lib/scrollbars";
 import { setupTopbarListeners } from "typedb-web-common/lib/topbar/setup-topbar-listeners";
 setupTopbarListeners();
-document.querySelectorAll(".nav .panels, .doc .listingblock .content pre").forEach(initCustomScrollbars);
+document.querySelectorAll(".doc .listingblock .content pre").forEach(initCustomScrollbars);
