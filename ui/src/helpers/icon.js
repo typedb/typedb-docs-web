fc1e6a847ae16855b5812c663d72bde065385d55
Alex Walker <alex@typedb.com>
2026-01-05 11:20:07 +0000

    Accept env var for icon path during build

--- ui/src/helpers/icon.js
+++ ui/src/helpers/icon.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 
 module.exports = name => {
-  const commonPath = `./ui/node_modules/typedb-web-common/src/icons/${name}.svg`;
+  const commonPath = (process.env["ICONS_PATH"] || `./ui/node_modules/typedb-web-common/src/icons`) + `/${name}.svg`;
   const svgContent = fs.readFileSync(commonPath, "utf-8");
   return svgContent;
 };
