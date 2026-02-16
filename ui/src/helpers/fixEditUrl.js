ca38c8cf1ebd16ddd9ff33c39e72bd6a507c0810
Alex Walker <alex@typedb.com>
2026-02-16 12:47:28 +0000

    Add "Edit on GitHub" button

--- ui/src/helpers/fixEditUrl.js
+++ ui/src/helpers/fixEditUrl.js
@@ -3,8 +3,8 @@
 module.exports = (...args) => {
   const numArgs = args.length;
   if (numArgs === 2 && args[0]) {
-    return args[0].replace('/development/content/', '/development/');
-  } 
+    return args[0].replace(/\/development\/content\/(\d+\.x)\//, '/$1-development/');
+  }
   if (numArgs < 2) throw new Error('{{fixEditUrl}} helper expects at least 1 arguments');
   return "";
 }
